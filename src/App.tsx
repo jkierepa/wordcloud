33917a2fc5e64e20c1517b0d0f3b4688481868d8
Jakub Kierepa <jakub.kierepa@gmail.com>
2021-09-14 18:55:46 +0200

    add login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Login from './pages/Login/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Login = () => {
+  const text = 'Login';
+  return <div>{text}</div>;
+};
+
+export default Login;
